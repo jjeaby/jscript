0740cca2d193935e2006edd3d1b71b715e0fb42c
jjeaby <32763196+jjeaby@users.noreply.github.com>
2020-09-29 11:34:02 +0900

    Create vmInit.sh

---
+++ vmInit.sh
@@ -0,0 +1,3 @@
+sudo apt update && sudo apt upgrade
+sudo apt install build-essential python python3 zsh nodejs 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

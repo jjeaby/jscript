65d4e5d2d78d7ed197b9fe1c57a5b2379d6f2e7a
jjeaby <32763196+jjeaby@users.noreply.github.com>
2020-09-29 12:49:09 +0900

    Update vmInit.sh

--- vmInit.sh
+++ vmInit.sh
@@ -1,3 +1,4 @@
 sudo apt update && sudo apt upgrade --yes
-sudo apt install build-essential git python python3 zsh nodejs --yes
+sudo apt install build-essential git python3 python3-pip zsh nodejs --yes
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo update-alternatives --install /usr/local/bin/python python /usr/bin/python3 

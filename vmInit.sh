bd98beb0e2f881aa45723351fa5e1a0909a75d71
jin <jjeaby@gmail.com>
2020-10-05 02:25:13 +0000

    python3-pip add

--- vmInit.sh
+++ vmInit.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade --yes
-sudo apt install build-essential git python3 python-pip zsh nodejs vim tmux --yes
+sudo apt install build-essential git python python-pip python3 python3-pip zsh nodejs vim tmux --yes
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
 pip install -U pip

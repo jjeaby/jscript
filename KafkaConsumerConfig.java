a49639a68c89fe5cbcc0a359b843c2fc49339f74
jjeaby <32763196+jjeaby@users.noreply.github.com>
2020-10-21 17:15:03 +0900

    Create KafkaConsumerConfig.java

---
+++ KafkaConsumerConfig.java
@@ -0,0 +1,59 @@
+package ml.jjeaby.kafka.config;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sds.ifm.common.KafkaConstants;
+import com.sds.ifm.common.config.props.IfmKafkaProps;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+@ConditionalOnMissingBean
+  public ConsumerFactory<String, String> consumerFactory() {
+
+    Map<String, Object> props = new HashMap<>();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1");
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "topic_group");
+    props.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 100000000);
+props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+    props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 20);
+    props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 600000);
+    props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, 1000);
+    props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 60000);
+
+    return new DefaultKafkaConsumerFactory<>(props);
+
+  }
+
+  @Bean(name="kafkaListenerContainerFactory")
+  public ConcurrentKafkaListenerContainerFactory<String, String>
+  kafkaListenerContainerFactory() {
+
+    ConcurrentKafkaListenerContainerFactory<String, String> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(consumerFactory());
+    factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+
+    return factory;
+  }
+}
